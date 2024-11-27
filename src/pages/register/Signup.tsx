ee98a06b6053c897cf3c411d99f3cc1948132168
shilamy <macmynev@gmail.com>
2024-11-27 17:41:54 +0300

    Creation of the registration form for new users

--- src/pages/register/Signup.tsx
+++ src/pages/register/Signup.tsx
@@ -5,8 +5,8 @@ import { RegisterForm } from "./form";
 const Signup = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
-    <div className="shadow-xl p-4 bg-white rounded-xl">
-        <h1 >Register</h1>
+    <div className="shadow-xl px-8 pb-8 pt-12 bg-white rounded-xl space-y-12">
+        <h1 className="font-semibold text-2xl mb-4">Create your Account</h1>
         <RegisterForm/>
     </div>
   </div>
--- src/pages/register/form.tsx
+++ src/pages/register/form.tsx
@@ -1,16 +1,27 @@
 'use client'
 
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
 export const RegisterForm = () =>{
     const onSubmit = (e:React.FormEvent) => {
  
         e.preventDefault()
-        console.log('Register')
+        console.log('Register!')
     }
     return(
-        <form>
-            <input/>
-            <input/>
-            <button>Register</button>
+        <form onSubmit={onSubmit} className="space-y-8 w-[400px]">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <label htmlFor="email">Email</label>
+                <Input type="email" id="email" placeholder="Email" />
+                </div>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                <label htmlFor="password">Password</label>
+                <Input type="password" id="password" placeholder="Password" />
+                </div>
+            <div className="w-full">
+            <Button className="w-full" size={'lg'}>Register</Button>
+            </div>
         </form>
     );
 
