aaaec7d569a430a02016b2eccd49fac6c7956975
shilamy <macmynev@gmail.com>
2024-11-27 17:51:43 +0300

    added a login link on the create account screen

--- src/pages/register/Signup.tsx
+++ src/pages/register/Signup.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "@/app/globals.css"
 import { RegisterForm } from "./form";
+import Link from "next/link";
+
 
 const Signup = () => {
   return (
@@ -8,6 +10,8 @@ const Signup = () => {
     <div className="shadow-xl px-8 pb-8 pt-12 bg-white rounded-xl space-y-12">
         <h1 className="font-semibold text-2xl mb-4">Create your Account</h1>
         <RegisterForm/>
+        <p className="text-center">
+          Have an Account? <Link className="text-charcoalGray" href="/Login">Sign In</Link></p>
     </div>
   </div>
   );
